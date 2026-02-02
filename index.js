ef6de88968db50ec85d2c5449c7de02638a521d4
javier garcia alvarez <javigarcial87@gmail.com>
2026-02-02 19:24:20 +0100

    swagger

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/config");
-
+const swaggerUI = require("swagger-ui-express");
+const swaggerSpec = require("./swagger");
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 const taskRoutes = require("./routes/tasks");
 
 require("dotenv").config();
